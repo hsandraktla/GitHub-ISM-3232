fd4b794a7d5ba15b4c6d7e97a8aa7e1551e7da1f
H Sandra Ktla <hsandraktla@usf.edu>
2025-01-22 23:01:17 -0500

    Task 2 - Product Details

--- Coding Challenge 1/cc_1.js
+++ Coding Challenge 1/cc_1.js
@@ -8,4 +8,17 @@ var isActive = true;
 // Log each variable's value and its type to the console using console.log() and typeof.
 console.log("Employee Name:", employeeName, "-", typeof employeeName);
 console.log("Employee ID:", employeeID, "-", typeof employeeID);
-console.log("Active Status:", isActive, "-", typeof isActive);)
+console.log("Active Status:", isActive, "-", typeof isActive);
+
+
+//Task 2: Product Details
+// Declare a variable productName using let and assign it a string value representing the product's name.
+let productName = "Wedding Dress";
+// Declare a variable productPrice using const and assgin it a numerical value representing the product's price.
+const productPrice = 3599;
+// Declare a variable isAvailable using var and assign it a boolean value indicating product availability.
+var isAvailable = false;
+// Log each variable's value and its type to the console.
+console.log("Product Name:", productName, "-", typeof productName);
+console.log("Product Price:", productPrice, "-", typeof productPrice);
+console.log("Product Availability;", isAvailable, "-", typeof isAvailable);
