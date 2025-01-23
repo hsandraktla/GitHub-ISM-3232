81c4f53a49a14452f2fd0723d3117fd2b2f7cc35
H Sandra Ktla <hsandraktla@usf.edu>
2025-01-22 22:46:11 -0500

    Task 1 - Employee Information

---
+++ Coding Challenge 1/cc_1.js
@@ -0,0 +1,11 @@
+// Task 1: Employee Information
+// Declare a variable employeeName using let and assign it the employee's name as a string.
+let employeeName = "Zachary Keenan";
+// Declare a variable employeeID using const and assign it the employee's ID as a number.
+const employeeID = 081303;
+// Declare a variable isActive using var and assign it a boolean value true to indicate the employee is currently active.
+var isActive = true;
+// Log each variable's value and its type to the console using console.log() and typeof.
+console.log("Employee Name:", employeeName, "-", typeof employeeName);
+console.log("Employee ID:", employeeID, "-", typeof employeeID);
+console.log("Active Status:", isActive, "-", typeof isActive);)
