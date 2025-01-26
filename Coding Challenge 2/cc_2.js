98a2eb4724f8eed24484143f3af92f146ca5af8a
H Sandra Ktla <hsandraktla@usf.edu>
2025-01-26 17:32:08 -0500

    Task 1 - Store Inventory

---
+++ Coding Challenge 2/cc_2.js
@@ -0,0 +1,9 @@
+//Task 1: Store inventory
+//Declare an array products using let and initialize it with at least five product names.
+let products = ["Dress", "Croptop", "Jeans", "Sweater", "Skirt"];
+//Add a new product to the array.
+products.unshift("Shoes");
+//Remove the last product from the array.
+products.pop("Shoes");
+//Log the updated product list to the console.
+console.log(products);
